f1821637bc26cba8a390c50ee32ec6790de096cc
Gevorg Aghakhanyan <gevorg@easydmarc.us>
2022-04-28 19:09:24 +0400

    Chat example

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.use(express.json())
+
+io.on('connection', (socket) => {
+    console.log('User connected')
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+
+    socket.on('chat message', ({msg, name}) => {
+        io.emit('chat message', {msg, name});
+    });
+});
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
